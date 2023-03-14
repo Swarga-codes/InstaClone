ad32d4dc7d64dbc21e7d3ca73a59de49b2541301
Swarga-codes <sd3526@srmist.edu.in>
2023-03-14 22:06:44 +0530

    added login functionality

--- Frontend/src/Components/Login/Login.js
+++ Frontend/src/Components/Login/Login.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 import { Link } from 'react-router-dom'
 import Logo from '../../assets/logo.png'
 function Login() {
+  const[email,setEmail]=useState('');
+  const[password,setPassword]=useState('');
+  const LoginUser = () =>{
+    fetch('http://localhost:5000/signin',{
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body: JSON.stringify({
+        email,
+        password
+      })
+    }).then(res=>res.json)
+    .then(data=>console.log('user authenticated successfully!'))
+    .catch(err=>console.log(err));
+  }
   return (
     <div className='login'>
     <div className="login_logo">
@@ -10,12 +26,12 @@ function Login() {
     </div>
     <div className="login_fields">
     <div>
-  <input type="email" name='email' id='email' placeholder='Enter email'/>
+  <input type="email" name='email' id='email' placeholder='Enter email' onChange={(e)=>setEmail(e.target.value)}/>
   </div>
   <div>
-  <input type="password" name='password' id='password' placeholder='Enter Password'/>
+  <input type="password" name='password' id='password' placeholder='Enter Password' onChange={(e)=>setPassword(e.target.value)}/>
   </div>
-  <button className='LoginBtn'>Login</button>
+  <button className='LoginBtn' onClick={LoginUser}>Login</button>
     <p className='noaccount'>Don't have an account? <Link to='/signup'><span>SignUp</span></Link></p>
     </div>
     </div>
